ac29e9ace81a08d4d0c76b0032aacea9f999b49c
vanfigo <rodrigo.loyola@live.com>
2019-04-08 15:50:14 -0500

    Gateway setup

--- currency-conversion-service/src/main/java/com/bucketdev/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/bucketdev/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,12 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+// 1 @FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+// 2 @FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    // 2 @GetMapping("currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 
 }
--- netflix-zuul-api-gateway-server/src/main/java/com/bucketdev/microservices/netflizzuulapigatewayserver/NetflizZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/bucketdev/microservices/netflizzuulapigatewayserver/NetflizZuulApiGatewayServerApplication.java
@@ -1,15 +1,17 @@
-package com.bucketdev.microservices.limitsservice;
+package com.bucketdev.microservices.netflizzuulapigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class LimitsServiceApplication {
+public class NetflizZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
+		SpringApplication.run(NetflizZuulApiGatewayServerApplication.class, args);
 	}
 
 }
