c0e05d6208b8dcd2df51a12a176285749d0676ee
vanfigo <rodrigo.loyola@live.com>
2019-04-08 13:21:55 -0500

    Eureka naming service, currency conversion service and load balancer configuration

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values
+(10001, 'USD', 'MXN', 19.35, 0),
+(10002, 'USD', 'EUR', 0.89, 0),
+(10003, 'USD', 'GBP', 0.77, 0);
